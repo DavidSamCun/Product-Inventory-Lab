5c732de917a9f81c10dc46178fc8946975116ae7
David <davidsamcun@gmail.com>
2022-03-29 22:49:44 -0400

    duumu

--- src/main/java/models/Tablet.java
+++ src/main/java/models/Tablet.java
@@ -1,4 +1,11 @@
 package models;
 
 public class Tablet {
+
+    private int id;
+    private String name;
+    private String brand;
+    private int qty;
+    private float price;
+
 }
--- src/test/java/models/LaptopTest.java
+++ src/test/java/models/LaptopTest.java
@@ -4,4 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LaptopTest {
 
+    private int id;
+    private String name;
+    private String brand;
+    private String type;
+    private int cores;
+    private int qty;
+    private float price;
+
 }
