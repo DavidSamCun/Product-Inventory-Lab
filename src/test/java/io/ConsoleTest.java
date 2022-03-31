83aa0e6b06f49921291fd3277fc12ba8e87b2e15
David <davidsamcun@gmail.com>
2022-03-31 00:54:04 -0400

    console work

---
+++ src/test/java/io/ConsoleTest.java
@@ -0,0 +1,13 @@
+package io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsoleTest {
+
+    @Test
+    void printWelcome() {
+        Console.printWelcome();
+    }
+}
